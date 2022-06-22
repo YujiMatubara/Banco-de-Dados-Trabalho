354978d1659f23cddb7f946d07ac4be23f9b14d5
PedroRaymundi <62019741+PedroRaymundi@users.noreply.github.com>
2022-06-22 20:48:54 -0300

    agora sim

--- Create Tables/createUsuario.sql
+++ Create Tables/createUsuario.sql
@@ -1,7 +1,18 @@
 create table USUARIO (
-    EMAIL varchar2(255),
-    NOME varchar2(100),
+    ID number generated always as identity,
+    EMAIL varchar2(255) not null,
+    NOME varchar2(100) not null,
     NIVEL_DE_PRIVILEGIO char default '0' not null,
-    constraint NIVEL_PRIV_CONSTRAINT check (NIVEL_DE_PRIVILEGIO in ('1', '0'))
-
+    NIVEL_DE_CONQUISTA number(4) default 0 not null,
+    
+    constraint NIVEL_PRIV_CONSTRAINT check (NIVEL_DE_PRIVILEGIO in ('1', '0')),
+    constraint EMAIL_UNIQUE unique(EMAIL),
+    constraint PK_USUARIO primary key(ID)
 );
+
+insert into USUARIO (EMAIL, NOME) values (
+    'teste@gmail.com',
+    'Teste Da Silva'
+);
+
+commit
