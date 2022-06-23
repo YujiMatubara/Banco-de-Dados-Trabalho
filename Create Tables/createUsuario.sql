940260ddffe32a3161d0988e5ce1ac80a3cb5765
PedroRaymundi <62019741+PedroRaymundi@users.noreply.github.com>
2022-06-22 21:05:18 -0300

    create table curso

--- Create Tables/createUsuario.sql
+++ Create Tables/createUsuario.sql
@@ -17,6 +17,24 @@ CREATE TABLE ASSUNTO (
     CONSTRAINT PK_ASSUNTO PRIMARY KEY (TEMA, SUB_TEMA)
 );
 
+create table CURSO (
+    ID number generated always as identity,
+    TITULO varchar2(100) not null,
+    DATA_LANCAMENTO date not null,
+    TEMA varchar2(50) not null,
+    SUB_TEMA varchar2(50) not null,
+    PRECO number(5) default 0,
+    NRO_AULAS number(4),
+    IDIOMA varchar2(20),
+    DURACAO varchar2(50),
+    SOMA_AVALIACAO number(7) default 0,
+    QTD_AVALIACAO number(7) default 0,
+    
+    constraint PK_CURSO primary key(ID),
+    constraint SK_CURSO unique(TITULO, DATA_LANCAMENTO),
+    constraint FK_CURSO foreign key(TEMA, SUB_TEMA) references ASSUNTO
+);
+
 insert into USUARIO (EMAIL, NOME) values (
     'teste@gmail.com',
     'Teste Da Silva'
