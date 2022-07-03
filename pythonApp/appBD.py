a274c4e3bfd44cf03e72a5200a008e2a33f5f2a7
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2022-07-03 16:49:42 -0300

    Changing instantclient

--- pythonApp/appBD.py
+++ pythonApp/appBD.py
@@ -2,6 +2,9 @@
 #abre a janela de ambientes em python e seleciona a que tem o pacote instalado
 #https://stackoverflow.com/questions/72015723/databaseerror-dpi-1047-cannot-locate-a-64-bit-oracle-client-library-running-c
 
+instantclient = r"C:\Users\Luisa\Documents\client_BD\instantclient_21_6"
+instantclient = "../../instantclient_21_6"
+
 import cx_Oracle
 from util.janelas import *
 from util.controleSGBD import *
@@ -9,7 +12,7 @@ from util.controleSGBD import *
 print("Iniciando conexao com servidor")
 #colocando o cliente do oracle
 #Colocar o path do instant client aqui
-cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\Luisa\Documents\client_BD\instantclient_21_6")
+cx_Oracle.init_oracle_client(lib_dir=instantclient)
 
 #entrando no sql
 enter_host = cx_Oracle.makedsn(host='orclgrad1.icmc.usp.br', port='1521', service_name='pdb_elaine.icmc.usp.br')
