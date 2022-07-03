13456a5128bed929d8ba27cf984c02f1803ba710
Wictor Dalbosco <56279618+WictorDalbosco@users.noreply.github.com>
2022-07-03 00:20:22 -0300

    novas queries

--- Create Tables/queries.sql
+++ Create Tables/queries.sql
@@ -1,6 +1,23 @@
----Questão 3
----internamente contabilizar todas as questões po ID
----com a tabela de respostas juntar com os temas, subtemas e nivel para ordenação
+--Query 1
+-- Selecionar médias de todos os cursos de um determinado tema que possuam pelo menos 50 avaliações ordenadas de maior para menor.
+-- (ou seja, queremos selecionar os cursos mais bem avaliados de cada tema) 
+Select titulo, SOMA_AVALIACAO/QTD_AVALIACAO as Media_Curso 
+from Curso join Assunto on Curso.tema = Assunto.tema
+where Curso.QTD_AVALIACAO >= 50
+group by(curso.titulo, curso.tema, SOMA_AVALIACAO/QTD_AVALIACAO)
+order by(SOMA_AVALIACAO/QTD_AVALIACAO) DESC;
+
+-- Query 2
+-- Selecionar de todos os usuários seu email, nome, número de cursos cursados, 
+-- número de cursos concluídos e número de conquistas de cada tipo (estatísticas dos usuários)
+
+
+
+-- Query  3
+-- Selecionar a quantidade de perguntas de todos os quizes de determinado nível de um mesmo tema/subtema (perguntas agrupadas por nível de cada tema)
+
+--internamente contabilizar todas as questões por ID
+-- com a tabela de respostas juntar com os temas, subtemas e nivel para ordenação
 select Q1.ID, Q1.TEMA, Q1.SUB_TEMA, Q1.NIVEL, Q2.NQUESTIONS
 from QUIZ Q1 join (
     select Q.ID, count(*) as NQUESTIONS
@@ -10,3 +27,14 @@ from QUIZ Q1 join (
         Q.ID) Q2
     on Q1.ID = Q2.ID
 order by Q1.TEMA, Q1.SUB_TEMA, Q1.NIVEL;
+
+-- Query 4
+-- Selecionar todos os palestrantes que deram os cursos mais bem avaliados de determinado tema (selecionar os melhores palestrantes de cada área) 
+
+
+
+-- Query 5
+-- Fazer ranking entre amigos com base na quantidade de conquistas ordenadas por conquistas de nível alto para baixo (tipo placar de medalhas das olimpíadas) 
+
+
+
