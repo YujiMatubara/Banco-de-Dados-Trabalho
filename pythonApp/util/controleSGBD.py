c765eeeaf8d9031d2d8aa3fe0ffca9321d6a1ba9
PedroRaymundi <62019741+PedroRaymundi@users.noreply.github.com>
2022-07-03 17:32:19 -0300

    removido o filtro adicional

--- pythonApp/util/controleSGBD.py
+++ pythonApp/util/controleSGBD.py
@@ -44,30 +44,3 @@ def pesquisa_cursos(cursor, nome_curso):
     else:
         print("Nenhum curso encontrado")
     return
-
-
-"""
-class FiltroCurso:
-	def __init__(titulo=None, tema=None, subtema=None, avaliacao=None):
-		self.tituo = titulo.upper()
-		self.tema = tema.upper()
-		self.subtema = subtema.upper()
-		self.avaliacao = int(avaliacao)
-
-	def gerar_clausula_where(self):
-		clausulas = []
-
-		if titulo is not None:
-			clausulas.append(f"upper(titulo) like '%{self.titulo}%'")
-		if tema is not None:
-			clausulas.append(f"upper(tema) like '%{self.tema}%'")
-		if subtema is not None:
-			clausulas.append(f"upper(subtema) like '%{self.subterma}%'")
-		if avaliacao is not None:
-			clausulas.append(f"case qtd_avaliacao when 0 then 0 else  soma_avaliacao / qtd_avaliacao end >= {self.avaliacao}")
-
-		if clausulas: # Test if clausulas is Empty
-			return ""
-		else:
-			return "where " + " and ".join(clausulas)
-"""
